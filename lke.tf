0deedf0bf64d0d5b2cd8128a985693d188a6b377
Suracha Chaokhunthod <sc@shinomin.me>
2020-12-15 10:42:10 +0700

    add versions

--- lke.tf
+++ lke.tf
@@ -1,15 +1,15 @@
 resource "linode_lke_cluster" "test_cluster" {
-  label = var.cluster_label
+  label       = var.cluster_label
   k8s_version = var.k8s_version
-  region = var.region
+  region      = var.region
   pool {
-    type = var.pool_type
+    type  = var.pool_type
     count = var.pool_count
   }
 }
 
 variable "cluster_label" {
-  type = string
+  type    = string
   default = "lke"
 }
 
@@ -18,17 +18,17 @@ variable "k8s_version" {
 }
 
 variable "region" {
-  type = string
+  type    = string
   default = "ap-south"
 }
 
 variable "pool_type" {
-  type = string
+  type    = string
   default = "g6-standard-1"
 }
 
 variable "pool_count" {
-  type = number
+  type    = number
   default = 1
 }
 
